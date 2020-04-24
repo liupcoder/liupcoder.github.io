406b1b1a667d767f24ac0f7ccc80aec87c7b49fb
daochun <>
2020-04-24 15:11:48 +0800

    暂存

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "title": "LP's Blog",
   "description": "一只iOS&前端程序猿",
   "dest": "public",
-  base : '/Blog/',
+  "base": '/Blog/',
   "head": [
     [
       "link",
@@ -21,19 +21,43 @@ module.exports = {
   ],
   "theme": "reco",
   "themeConfig": {
+    type: 'blog',
+    "themePicker": false,
     "nav": [
       {
-        "text": "Home",
-        "link": "/",
-        "icon": "reco-home"
-      },
-      {
-        "text": "TimeLine",
+        "text": "时间",
         "link": "/timeline/",
         "icon": "reco-date"
       },
       {
-        "text": "Contact",
+        "text": "笔记",
+        "link": "/categories/note/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "文档",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "Vue",
+            "link": "https://cn.vuejs.org/v2/guide/",
+            // "icon": "reco-github"
+          }
+        ]
+      },
+      {
+        "text": "工具",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "Vue",
+            "link": "https://cn.vuejs.org/v2/guide/",
+            // "icon": "reco-github"
+          }
+        ]
+      },
+      {
+        "text": "链接",
         "icon": "reco-message",
         "items": [
           {
@@ -48,32 +72,43 @@ module.exports = {
     "blogConfig": {
       "category": {
         "location": 2,
-        "text": "Category"
+        "text": "分类"
       },
       "tag": {
         "location": 3,
-        "text": "Tag"
-      }
+        "text": "标签"
+      },
+      "note": {
+        "location": 4,
+        "text": "笔记"
+      },
     },
     "friendLink": [
-      {
-        "title": "午后南杂",
-        "desc": "Enjoy when you can, and endure when you must.",
-        "email": "1156743527@qq.com",
-        "link": "https://www.recoluan.com"
-      }
+      // {
+      //   "title": "午后南杂",
+      //   "desc": "Enjoy when you can, and endure when you must.",
+      //   "email": "1156743527@qq.com",
+      //   "link": "https://www.recoluan.com"
+      // }
     ],
-    "logo": "/logo.png",
+    "logo": "",
     "search": true,
     "searchMaxSuggestions": 10,
     "sidebar": "auto",
     "lastUpdated": "Last Updated",
     "author": "LP",
     "authorAvatar": "/avatar.jpg",
-    "record": "xxxx",
-    "startYear": "2017"
+    "record": "",
+    "mode": "light",
+    "startYear": "2020"
   },
   "markdown": {
     "lineNumbers": true
-  }
+  },
+  plugins: [
+    ["flowchart"], // 支持流程图
+    ["vuepress-plugin-smooth-scroll"], // 平滑滚动
+    ["@vuepress/nprogress"], // 加载进度条
+    ["reading-progress"] // 阅读进度条
+  ]
 }
