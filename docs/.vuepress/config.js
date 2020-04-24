b813e52a2ca15f6354b5ff4713b9c88bcfc5c100
daochun <>
2020-04-24 11:46:48 +0800

    测试

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -60,12 +60,6 @@ module.exports = {
         "desc": "Enjoy when you can, and endure when you must.",
         "email": "1156743527@qq.com",
         "link": "https://www.recoluan.com"
-      },
-      {
-        "title": "vuepress-theme-reco",
-        "desc": "A simple and beautiful vuepress Blog & Doc theme.",
-        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
-        "link": "https://vuepress-theme-reco.recoluan.com"
       }
     ],
     "logo": "/logo.png",
