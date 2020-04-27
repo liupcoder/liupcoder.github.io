c8eb77e31712d323f79dc9125e2c2970ac6a4aae
daochun <>
2020-04-27 10:19:28 +0800

    每日一题更新

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ const sidebar = {
       title: '每日一题',
       collapsable: true,
       children: [
-        // 'IDEA前端配置',
+        '200427',
       ]
     }
   ]
@@ -38,6 +38,7 @@ const nav = [
         text: '在线快捷', items: [
           { text: '代码格式化', link: 'https://tool.oschina.net/codeformat/html' },
           { text: '编码转换', link: 'http://tool.chinaz.com/tools/unicode.aspx' },
+          { text: 'Babel', link: 'https://www.babeljs.cn/repl' },
           { text: 'PDF转换', link: 'https://smallpdf.com/cn/pdf-to-word' },
           { text: 'MD编辑器', link: 'https://www.zybuluo.com/mdeditor' },
           { text: 'MD表格生成', link: 'https://tableconvert.com/?output=text' },
