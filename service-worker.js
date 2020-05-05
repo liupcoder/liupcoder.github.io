2cacb45bb96cdab00b57f5131ef9822633b701ce
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-05 01:51:28 +0000

    Deploy liupcoder/Blog to github.com/liupcoder/Blog.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -27,7 +27,7 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "68f3b1c906fb8ff8aca8ae00c115c9a6"
+    "revision": "69f6f60251712789eae344aa5b2eabd4"
   },
   {
     "url": "assets/css/0.styles.cb7afac4.css",
@@ -134,8 +134,8 @@ self.__precacheManifest = [
     "revision": "76393824282440aadbfb49958599cad4"
   },
   {
-    "url": "assets/js/app.7357047d.js",
-    "revision": "04b81c03b242eea5ac27bccb937e8ba1"
+    "url": "assets/js/app.c6cfc35a.js",
+    "revision": "8051bf096bf5e4dc454fb7438c0b7125"
   },
   {
     "url": "assets/js/vendors~flowchart.b17edc10.js",
@@ -155,91 +155,91 @@ self.__precacheManifest = [
   },
   {
     "url": "categories/index.html",
-    "revision": "d8fac6ec7c67ca663e52f2a6cdf6b32d"
+    "revision": "c38cc209b8c7dea80393affee385122f"
   },
   {
     "url": "categories/iOS/index.html",
-    "revision": "3da30780779f4c6083c0d006e918e420"
+    "revision": "ce94afd8b0fb54c20d3f0cd286a22ea7"
   },
   {
     "url": "categories/前端/index.html",
-    "revision": "35a5257a689128c74acd196ef7e68d54"
+    "revision": "9ab26fae07cc3ea43c5445cd4b03cea3"
   },
   {
     "url": "categories/理财/index.html",
-    "revision": "1389797538bd7e3f92ab9740f95ef04c"
+    "revision": "7fd38a8389b0136117f9683440bfa56d"
   },
   {
     "url": "index.html",
-    "revision": "1ea3662a8df3ac46a0984b6804d34968"
+    "revision": "8efaae17aefca8e2f0991a03383885a1"
   },
   {
     "url": "tag/IDEA/index.html",
-    "revision": "9cca960ede9dae4e2dbf9f60c4848f49"
+    "revision": "29df1d167d4ae5892ad432c8501e2359"
   },
   {
     "url": "tag/index.html",
-    "revision": "2356276a4d0f321056e43764df1ecc97"
+    "revision": "fe62b83d207295df06f40d2a8ec739f7"
   },
   {
     "url": "tag/iOS/index.html",
-    "revision": "23f825353265f9dfe7122cc07c86ee47"
+    "revision": "d59291404069d4b38443a0a0350dce86"
   },
   {
     "url": "tag/基金/index.html",
-    "revision": "dd512669a06cb37734731f4beaa709b6"
+    "revision": "76cd71a76ddccf4ebd964e1fb1e34c28"
   },
   {
     "url": "timeline/index.html",
-    "revision": "9b249e422103a11da0d338ea19e31287"
+    "revision": "892773b010988c7d0a46a8a362bda69a"
   },
   {
     "url": "views/financial/19061601.html",
-    "revision": "468b30b45d1f88e4fd47435bedffd249"
+    "revision": "8b8bef2e27698a297c18d4bbb9125741"
   },
   {
     "url": "views/interview/ES6中this指向.html",
-    "revision": "ed220576546800335d230f55794499e0"
+    "revision": "bcec784b6f8544032afeffce9450aeb6"
   },
   {
     "url": "views/interview/ES6类的理解.html",
-    "revision": "ca0c524c7121758d3af084a2f7b94909"
+    "revision": "a98993e4198b92ab2fc3242eb3b5d5af"
   },
   {
     "url": "views/interview/index.html",
-    "revision": "395311a2d126dbf90f7691794f9bb034"
+    "revision": "27f3b0d30a6649a10e0f5790fbe76f10"
   },
   {
     "url": "views/interview/webpack构建后this指向.html",
-    "revision": "c16fc4040fcb99987a2892d520284a95"
+    "revision": "942a24a3084b5e19551544f76db3a49a"
   },
   {
     "url": "views/interview/伪数组转数组.html",
-    "revision": "bcdfc6579e793117e415db5ca2bbe387"
+    "revision": "592b8e0716e478e34c2872bf4324e0ba"
   },
   {
     "url": "views/interview/变量声明.html",
-    "revision": "f2dadc2d1134e658b9388a330f8e7621"
+    "revision": "b216c9665449a14132564c021ae71705"
   },
   {
     "url": "views/iOS/2016/iOS字面量的简单使用.html",
-    "revision": "e7b3c904a8491cf9409745a4048ddd5b"
+    "revision": "9a58bb7514f20eafd72761cd9c4370b3"
   },
   {
     "url": "views/Web/2019/IDEA前端配置.html",
-    "revision": "5e724bf838a83f3d37852a90edf2c7e8"
+    "revision": "ff5acc3030b1af7db01c0e61089dd174"
   },
   {
     "url": "views/Web/2020/前端知识体系汇总.html",
-    "revision": "03a20140c0bf29a79a9f986b0ee46bf6"
+    "revision": "f6cdebdae5aa24ca6d59bbc4537ef48a"
   },
   {
     "url": "views/Web/2020/常用工具.html",
-    "revision": "716630dcd12da7d4d8211fb2569a9a95"
+    "revision": "b000919792bb4efa45311b41ac8720a7"
   },
   {
     "url": "views/Web/2020/支付分享.html",
-    "revision": "1aa36e8cae5c6c526fa1449a17558949"
+    "revision": "bfa18456d44d690ac2bfb39f6ac672a8"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
